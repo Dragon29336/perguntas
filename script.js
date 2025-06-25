e2ca9744504d4ff0b9d8413e55620a602fda9a14
Dragon29336 <bruno.lotoski@escola.pr.gov.br>
2025-06-25 12:06:40 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+const perguntas =[
+  {
+    enunciado:"1" ,
+    alternativas[
+  {
+    texto"" ,
+    afirmação""
+  },
+      {
+        texto"" ,
+        afirmação""
+      }
+      ]
+      },
+      {
+         enunciado:"2" ,
+    alternativas[
+  {
+    texto"" ,
+    afirmação""
+  },
+      {
+        texto"" ,
+        afirmação""
+      }
+      ]
+      },
+       {
+          enunciado:"3" ,
+    alternativas[
+  {
+    texto"" ,
+    afirmação""
+  },
+      {
+        texto"" ,
+        afirmação""
+      }
+      ]
+       },
+       {
+         enunciado:"4" ,
+    alternativas[
+  {
+    texto"" ,
+    afirmação""
+  },
+      {
+        texto"" ,
+        afirmação""
+      }
+      ]
+      },
+      {
+         enunciado:"5" ,
+    alternativas[
+  {
+    texto"" ,
+    afirmação""
+  },
+      {
+        texto"" ,
+        afirmação""
+      }
+      ]
+      },
+        {
+       enunciado:"6" ,
+    alternativas[
+  {
+    texto"" ,
+    afirmação""
+  },
+      {
+        texto"" ,
+        afirmação""
+      }
+      ]
+      },
