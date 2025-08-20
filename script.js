5a79d1fc4b80259ab43a293923c5e9268e8281f5
Dragon29336 <bruno.lotoski@escola.pr.gov.br>
2025-08-20 11:49:42 -0300

    .

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const perguntas =[
     afirmação"Você é objetivo(a) e direto(a), comunicando com clareza aquilo que realmente sabe. "
   },
       {
-        texto"contando histórias e enrolando nas dúvidas sobre você" ,
+        texto"B) contando histórias e enrolando nas dúvidas sobre você" ,
         afirmação"Você prefere contar histórias longas e, quando não tem certeza sobre algo, acaba enrolando nas respostas para ter mais glamor."
       }
       ]
@@ -23,10 +23,10 @@ const perguntas =[
     alternativas[
   {
     texto"A) Sim, procuro sempre aprender mais e melhorar minhas habilidades" ,
-    afirmação"Está sempre em busca de aprender mais e se desenvolver continuamente."
+    afirmação"B) Está sempre em busca de aprender mais e se desenvolver continuamente."
   },
       {
-        texto"Sim, mas apenas oq é exigido" ,
+        texto"B) Sim, mas apenas oq é exigido" ,
         afirmação"Busca melhorar apenas o que é necessário sem novos conhecimentos."
       }
       ]
@@ -35,11 +35,11 @@ const perguntas =[
           enunciado:"3)oque é mais valorizado por você na organização do trabalho?" ,
     alternativas[
   {
-    texto"trabalho em equipe" ,
+    texto"A) trabalho em equipe" ,
     afirmação"Valoriza o trabalho em equipe e acredita na força da equipe"
   },
       {
-        texto"competição entre colegas" ,
+        texto"B) competição entre colegas" ,
         afirmação"se importa com o ser unico e não com o resultado"
       }
       ]
@@ -48,11 +48,11 @@ const perguntas =[
          enunciado:"4) Valoriza a competição entre colegas como forma de se destacar." ,
     alternativas[
   {
-    texto"Transparência na comunicação" ,
+    texto"A) Transparência na comunicação" ,
     afirmação"Acredita que a transparência na comunicação é essencial para um bom ambiente"
   },
       {
-        texto"hierarquia rígidas" ,
+        texto"B) hierarquia rígidas" ,
         afirmação"Acredita que hierarquias rígidas são fundamentais para manter a ordem no trabalho."
       }
       ]
@@ -61,11 +61,11 @@ const perguntas =[
          enunciado:") 5)Se você cometesse um erro no trabalho, oque você faria? :" ,
     alternativas[
   {
-    texto"Esconder o erro e tentar resolver sozinho" ,
+    texto"A) Esconder o erro e tentar resolver sozinho" ,
     afirmação"se mantém isolado evitando o julgamento e a ideia de errar"
   },
       {
-        texto"Assumir e comunicar ao superiores" ,
+        texto"B) Assumir e comunicar ao superiores" ,
         afirmação"entender oque fez e buscar acertar ou arrumar o erro"
       }
       ]
@@ -74,11 +74,11 @@ const perguntas =[
        enunciado:"6) Na hora de buscar feedback sobre o desempenho, o que você prioriza?" ,
     alternativas[
   {
-    texto"ouve críticas" ,
+    texto"A) ouve críticas" ,
     afirmação"Aceita críticas construtivas e usa isso para melhorar"
   },
       {
-        texto"se mantém em suas próprias ideias" ,
+        texto"B) se mantém em suas próprias ideias" ,
         afirmação"Prefere sempre defender o próprio ponto de vista diante de críticas."
       }
       ]
